ade502ffc31852a4f20969dc8abc0663dd8319fe
ivoshet <ivoshet.world@gmail.com>
2020-04-16 22:34:44 +0700

    added css module

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Layout from './hoc/Layout/Layout';
 function App() {
     return(
         <Layout>
-            <div style={{width: '400', border: '1px solid black'}}>
+            <div style={{width: '400px', border: '1px solid black'}}>
                 <h1>
                     Layout works
                 </h1>
